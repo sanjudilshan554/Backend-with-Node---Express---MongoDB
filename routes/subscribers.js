2383a5a6548ebe6179e3ffa6d76a6aad400f4b50
Sanju Dilshan <sanjudilshan554@gmail.com>
2025-02-22 09:55:12 +0530

    save a recode

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,20 +1,36 @@
 const express = require('express')
 const router = express.Router()
 
-// Getting All
-router.get('/', (req, res) => {
+const Subscriber = require('../models/subscriber')
 
-    res.send('Hi there')
+// Getting All
+router.get('/', async (req, res) => {
+    try{
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    }catch(err){
+        res.json({ message: err.message })
+    }
 
 })
-// Getting One
-router.get('/:id', (req, res) => {
 
+// Getting One
+router.get('/:id', async (req, res) => {
+    
 })
 
 // Creating One
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscriber: req.body.subscriber
+    })
+    try{
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    }catch(err){
+        res.status(400).json({ message: err.message})
+    }
 })
 
 // Updating One
